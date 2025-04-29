2b202aeb98675ff672a78f6dd7d09a3be97713b2
Nicole Mubarak <nik4mub@gmail.com>
2025-04-29 15:54:03 -0500

    this is just an example

---
+++ testing/script.sh
@@ -0,0 +1,7 @@
+mkdir coolbeans
+touch maxistryingme.txt
+echo "This is a test file." > maxistryingme.txt
+mv maxistryingme.txt thisshouldhaveabetter.txt
+mv thisshouldhaveabetter.txt coolbeans/
+cd coolbeans
+cat thisshouldhaveabetter.txt
