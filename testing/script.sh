6a439b1e3491bc89c4461115cc5d9cbf0518a92a
David D. Smith <smith74066@att.net>
2026-02-26 17:13:03 +0000

    update 2/23/26

--- testing/script.sh
+++
@@ -1,7 +0,0 @@
-mkdir coolbeans
-touch maxistryingme.txt
-echo "This is a test file." > maxistryingme.txt
-mv maxistryingme.txt thisshouldhaveabetter.txt
-mv thisshouldhaveabetter.txt coolbeans/
-cd coolbeans
-cat thisshouldhaveabetter.txt
